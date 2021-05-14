7120fd98ff72c1c3b37950659f73675d83a63cc0
gearprn <gearprn@gmail.com>
2021-05-14 16:12:14 +0700

    :wrench: Update sign in page

--- clapper/src/pages/SignInPage.js
+++ clapper/src/pages/SignInPage.js
@@ -1,55 +1,76 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
+import { NavLink } from 'react-router-dom';
 
-function SignInPage({ Signin, error }) {
-  const [details, setDetails] = useState({ name: '', email: '', password: '' });
+const SignInPage = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = useCallback((event) => {
+    setUsername(event.target.value);
+  }, []);
+
+  const handlePasswordChange = useCallback((event) => {
+    setPassword(event.target.value);
+  }, []);
 
   const submitHandler = (e) => {
     e.preventDefault();
-
-    Signin(details);
+    console.log(username, password);
   };
 
   return (
-    <div className="flex h-screen items-center justify-center bg-gray-800">
+    <div className="flex h-screen items-center justify-center bg-black text-white">
       <form onSubmit={submitHandler}>
-        <div>
-          <h2 className="font-sans text-white">Sign in</h2>
-          {error != '' ? <div className="error">{error}</div> : ''}
-          <div className="font-sans text-white">
-            <label htmlFor="name">Name:</label>
-            <input
-              type="text"
-              name="name"
-              id="name"
-              onChange={(e) => setDetails({ ...details, name: e.target.value })}
-              value={details.name}
-            />
-          </div>
-          <div className="font-sans text-white">
-            <label htmlFor="email">Email:</label>
+        <div className="p-10 rounded bg-gray-800">
+          <h2 className="text-xl font-semibold">Sign in</h2>
+          <p>fill in username and password to enjoy Clapper</p>
+
+          <div className="mt-10">
+            <label htmlFor="email">Email</label>
             <input
               type="email"
               name="email"
               id="email"
-              onChange={(e) => setDetails({ ...details, email: e.target.value })}
-              value={details.email}
+              className="border-0 w-full border-b-2 bg-gray-800 p-2"
+              onChange={handleUsernameChange}
             />
           </div>
-          <div className="font-sans text-white">
-            <label htmlFor="password">Password:</label>
+
+          <div className="mt-2">
+            <label htmlFor="password">Password</label>
             <input
               type="password"
               name="password"
               id="password"
-              onChange={(e) => setDetails({ ...details, password: e.target.value })}
-              value={details.password}
+              className="border-0 w-full border-b-2 bg-gray-800 p-2"
+              onChange={handlePasswordChange}
             />
           </div>
-          <input type="submit" value="Sign in" />
+
+          <div className="mt-16">
+            <div className="flex justify-center items-center">
+              <div>
+                <button className="w-full font-semibold bg-transparent hover:border-gray-500 py-2 px-4 border-2 border-gold-500 hover:border-transparent rounded">
+                  Sign in
+                </button>
+                <div className="text-sm text-center mt-5 block">
+                  New to Clapper? {''}
+                  <NavLink to="/signup" className="underline text-gray-200">
+                    SIGN UP
+                  </NavLink>
+                </div>
+                <div className="text-center mt-4">
+                  <NavLink to="/" className="underline text-gray-200">
+                    Home
+                  </NavLink>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </form>
     </div>
   );
-}
+};
 
 export default SignInPage;
