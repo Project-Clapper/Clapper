b71e70bdc6887064e115868e9ad639f7c50eaca6
gearprn <gearprn@gmail.com>
2021-05-17 05:30:09 +0700

    :fire: Remove unused variable

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage';
 import SignInPage from './pages/SignInPage';
