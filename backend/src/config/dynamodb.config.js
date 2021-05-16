efead53d67f982018a146b043622f7d5783ff205
gearprn <gearprn@gmail.com>
2021-05-17 02:58:22 +0700

    :wrench: Add dynamodb config

---
+++ backend/src/config/dynamodb.config.js
@@ -0,0 +1,11 @@
+import AWS from 'aws-sdk';
+
+AWS.config.update({
+  region: process.env.AWS_REGION,
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_KEY,
+});
+
+const dynamoClient = new AWS.DynamoDB.DocumentClient();
+
+export { dynamoClient };
