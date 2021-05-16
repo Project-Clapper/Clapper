0886105ad43cb132e922eaf45b52b9124eac0007
gearprn <gearprn@gmail.com>
2021-05-17 02:58:44 +0700

    :wrench: Update auth route

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import { signUp, signIn } from '../controllers/auth.controller';
+import { signUp, signIn, me } from '../controllers/auth.controller';
 
 const router = express.Router();
 
 router.post('/signup', signUp);
 router.post('/signin', signIn);
-router.get('/me');
+router.get('/me', me);
 
 export default router;
