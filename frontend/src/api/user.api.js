007e21237f5196a561d2311eb503e048bb4cdbbe
gearprn <gearprn@gmail.com>
2021-05-18 15:44:05 +0700

    :wrench: Add user api

---
+++ frontend/src/api/user.api.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const api_endpoint = process.env.REACT_APP_API_ENDPOINT;
+
+const userCommunities = (clientId) => {
+  return axios.get(`${api_endpoint}user/communities?clientId=${clientId}`);
+};
+
+const updateUser = (clientId, profileImage, bannerImage) => {
+  console.log(clientId, profileImage, bannerImage);
+  return axios.post(`${api_endpoint}user/update`, { clientId, profileImage, bannerImage });
+};
+
+export { userCommunities, updateUser };
